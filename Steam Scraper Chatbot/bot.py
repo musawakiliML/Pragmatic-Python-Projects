be10f48ee707ba4653685c3c3f795fd52bf23ffb
musawakiliML <musawakiliml@hotmail.com>
2022-07-31 12:22:29 +0100

    Create a simple response bot to test

--- Steam Scraper Chatbot/bot.py
+++ Steam Scraper Chatbot/bot.py
@@ -1,13 +1,27 @@
-from crypt import methods
+# Required libaries
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+
+# User defined functions
 from scrape import popular_release
 
 
-# initializing flask app
+# Initializing flask app
 app = Flask(__name__)
 
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    pass
+    # Getting user message and Creating response instance
+    incoming_msg = request.values.get('Body', '').lower()
+    bot_response = MessagingResponse()
+    bot_message = bot_response.message()
+
+    if incoming_msg == "hello":
+        bot_message.body("Hello, I am Bot!!")
+
+    return str(bot_response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
