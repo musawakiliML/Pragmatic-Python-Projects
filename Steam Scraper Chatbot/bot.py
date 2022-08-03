236babbf936efccb4c479b623ff45ddd8e3ff909
musawakiliML <musawakiliml@hotmail.com>
2022-08-03 13:45:50 +0100

    Update bot.py

--- Steam Scraper Chatbot/bot.py
+++ Steam Scraper Chatbot/bot.py
@@ -17,13 +17,20 @@ def bot():
     incoming_msg = request.values.get('Body', '').lower()
     bot_response = MessagingResponse()
     bot_message = bot_response.message()
+
+    # Bot commands
     command = ['hello', 'hi', 'what do you do', 'start', 'commands']
+    bot_commands_list = ['/get_popular_releases','/get_new_releases','get_new_releases','get_popular_releases', 'get popular releases', 'get new releases']
+    
+    # bot navigation and conversation
     if incoming_msg in command:
         bot_commands = f"""Hello, I am Steam Bot. I Scrape the Steam Game Store Website to get games and more. I Can you anything on the site just search through my commands and hit me {em(':smile:', language="alias")}.
-        These are my commands:
+        These are my commands {em(":robot:", language="alias")} :
         - /get_popular_releases
         - /get_new_releases
         """
+       
+    
         bot_message.body(bot_commands)
         # bot_message.body(str(len(h)))
 
