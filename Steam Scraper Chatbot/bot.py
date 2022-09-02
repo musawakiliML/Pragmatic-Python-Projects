2db8e93c395e070f8c6e199304d1ad7569131776
musawakiliML <musawakiliml@hotmail.com>
2022-09-02 01:17:14 +0100

    added flask file serving functions

--- Steam Scraper Chatbot/bot.py
+++ Steam Scraper Chatbot/bot.py
@@ -1,5 +1,5 @@
 # Required libaries
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from twilio.twiml.messaging_response import MessagingResponse
 from emoji import emojize as em
 
@@ -9,6 +9,8 @@ from scrape import popular_release, get_steam_image
 
 # Initializing flask app
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/bot', methods=['POST'])
@@ -30,12 +32,24 @@ def bot():
         - /get_popular_releases
         - /get_new_releases 
         - /get_"""
-
-        bot_message.body(bot_commands)
-        bot_message.media(get_steam_image())
+        link = 'http://www.africau.edu/images/default/sample.pdf'
+        y_link = 'https://www.youtube.com/watch?v=YslpixMUbYU'
+        # bot_message.body(bot_commands)
+        # bot_message.media(get_steam_image())
+        # bot_message.media(link)
+        # bot_message.body(y_link)
+        # bot_message.media('https://download.samplelib.com/mp3/sample-3s.mp3')
         # bot_message.body(str(len(h)))
+        # bot_message.media(
+        # 'https://890a-197-210-52-107.ngrok.io/uploads/output_file.json')
 
     return str(bot_response)
+
+
+@app.route('/uploads/<path:filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename, as_attachment=True)
 
 
 if __name__ == "__main__":
